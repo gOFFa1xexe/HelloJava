65aca15949f853570140ed17d79f91e38ab0398c
gOFFa1xexe <lol228gofa@gmail.com>
2022-08-23 18:13:06 +0300

    Lab1.Ropnitskyi

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String msg = "Ropnitskyi Rostyslav KI-впавап";
+        String msg = "Ropnitskyi Rostyslav KI-33";
         System.out.println();System.out.println(msg);
     }
 
